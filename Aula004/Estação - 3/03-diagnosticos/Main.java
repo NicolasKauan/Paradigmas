055ca061ec69115719a6cb4888411b47d4060aa2
Nicolas Kauan <166877999+NicolasKauan@users.noreply.github.com>
2026-08-31 22:02:15 -0300

    Atividade da aula 004

---
+++ Aula004/Estação - 3/03-diagnosticos/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    static final String ALUNO = "Nicolas Kauan Vieira Da Silva";
+    static final String RA = "24191533-2";
+
+    public static void main(String[] args) {
+        System.out.println("ALUNO: " + ALUNO + " | RA: " + RA);
+
+        int total = 10 + 20 // CORRIGIR 1: acrescente o ponto e virgula
+
+        if (total > 20 { // CORRIGIR 2: feche corretamente o parenteses
+            System.out.println("Programa corrigido!"); // CORRIGIR 3: feche o texto entre aspas
+            System.out.println("Resultado: " + total);
+        }
+    }
+}
